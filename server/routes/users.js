c8586cf6995718d08cb052e1d2dd9cf59d576c86
Nathen St. Germain <natesaint97@gmail.com>
2018-07-18 16:33:29 -0400

    Working on crud operations in api

--- server/routes/users.js
+++ server/routes/users.js
@@ -12,6 +12,7 @@ var router = express.Router();
  * Response:
  * All user objects in db displayed with this format
  *     {
+ *         _id: "id",
  *         username: "username",
  *         name: "name",
  *         email: "email"
@@ -21,6 +22,46 @@ router.get('/', function(req, res) {
   User.find({}, function(err, users) {
     if (err) throw err;
     res.status(200).send(users);
+  });
+});
+
+/**
+ * GET a specific user in the database by username
+ *
+ * Example: GET /api/users/bob
+ * Response:
+ * Returns JSON of user matching the username or nothing if no match
+ *     {
+ *         _id: "id",
+ *         username: "bob",
+ *         name: "Robert Bobson",
+ *         email: "bob@email.com"
+ *     }
+ */
+router.get('/:username', function(req, res) {
+  User.findOne({username: req.params.username}, function(err, user) {
+    if (err) throw err;
+    res.status(200).send(user);
+  });
+});
+
+/**
+ * Update user password, indentified by id
+ *
+ * Example: PUT /api/users/1234
+ * Response:
+ * Returns JSON of user that was updated
+ *     {
+ *         _id: "id",
+ *         username: "username",
+ *         name: "name",
+ *         email: "email"
+ *     }
+ */
+router.put('/:user_id', function(req, res) {
+  User.findByIdAndUpdate(req.params.user_id, req.body, function(err, user) {
+    if (err) throw err;
+    res.status(200).send(user);
   });
 });
 
